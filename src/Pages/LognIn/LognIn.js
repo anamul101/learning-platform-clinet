955cdf3c335a591f7375fa74ea6119234d58f684
anamul101 <rjanamul01871@gmail.com>
2022-10-25 07:02:56 +0100

    Components Setup

---
+++ src/Pages/LognIn/LognIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LognIn = () => {
+    return (
+        <div>
+            <h1>this is lognin</h1>
+        </div>
+    );
+};
+
+export default LognIn;
---
+++ src/Routes/PrivetRouters/PrivetRouters.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrivetRouters = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default PrivetRouters;
